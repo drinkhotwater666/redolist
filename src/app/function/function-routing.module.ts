fccb3aa0aa7939f466bc9121aca6b1cd7c889dfe
drinkhotwater666 <584324847@qq.com>
2021-12-29 22:47:35 +1030

    show todo in css

--- src/app/function/function-routing.module.ts
+++ src/app/function/function-routing.module.ts
@@ -2,15 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CheckboxComponent } from './checkbox/checkbox.component'
 import { DragComponent } from './drag/drag.component'
-
+import { TodoComponent } from '../todos/todo/todo.component'
 const routes: Routes = [
+  // {
+  //   path: 'drag',
+  //   component: DragComponent
+  // },
+  // {
+  //   path: 'checkbox',
+  //   component: CheckboxComponent
+  // },
   {
     path: 'drag',
-    component: DragComponent
-  },
-  {
-    path: 'checkbox',
-    component: CheckboxComponent
+    component: TodoComponent
   }
 ];
 
